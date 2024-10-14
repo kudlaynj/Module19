9bfe73bc008d964f6cfacd6cc59de83f1e072a9a
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-10-14 22:25:02 +0300

    Update admin.py

--- task1/admin.py
+++ task1/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Buyer)
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'description',)
+    # fields = [('title', 'description',)]
+    search_fields = ('title',)
+    list_filter = ('cost',)
+    fieldsets = (
+        ('info, ', {
+            'fields':
+                ('title', 'cost',)
+        }),
+        ('footer', {
+            'fields':
+                ('description',)
+        })
+
+    )
